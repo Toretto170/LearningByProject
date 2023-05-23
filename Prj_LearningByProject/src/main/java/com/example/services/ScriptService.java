9240b0feaf1fc7444345c9987924bb3562cb0b47
ufiore <119484794+ufiore@users.noreply.github.com>
2023-05-23 12:25:30 +0200

    ho cancellato le query scritte tramite Java

--- Prj_LearningByProject/src/main/java/com/example/services/ScriptService.java
+++ Prj_LearningByProject/src/main/java/com/example/services/ScriptService.java
@@ -7,15 +7,8 @@ import com.example.entities.Parola;
 
 public interface ScriptService {
 	
-	List<String> getParola();
-	Parola getParolaById (int id);
 	void creaParola(String parola, Frase frase);
-	
-	List<String> getFrase();
+
 	void creaFrase(String frase);
-	Frase getFraseById (int id);
-	List<String> getFrasiInTesto();
-	List<String> getParoleInTesto();
-	int numeroParoleInFrase(String paroleInFrase);
 
 }
--- Prj_LearningByProject/src/main/java/com/example/services/ScriptServiceImpl.java
+++ Prj_LearningByProject/src/main/java/com/example/services/ScriptServiceImpl.java
@@ -28,62 +28,6 @@ public class ScriptServiceImpl implements ScriptService{
 	EntityManager entityManager;
 	
 	@Override
-	public List<String> getParola() {
-		
-		Set<String> parole = new TreeSet<>();
-		
-		for (String p : getParola()) {
-			parole.add(p.getParoleInFrase());
-		}
-		
-		return new ArrayList<>(parole);
-	}
-
-	@Override
-	public Parola getParolaById(int id) {
-		return parolaDao.getReferenceById(id);
-	}
-
-	@Override
-	public List<String> getTestoParola() {
-		return null;
-	}
-
-	@Override
-	public List<String> getFrase() {
-		Set<String> frasi = new TreeSet<>();
-		
-		for (String f : getFrase()) {
-			frasi.add(f.getFraseinTesto);
-		}
-		
-		return new ArrayList<>(frasi);
-	}
-
-	@Override
-	public Frase getFraseById(int id) {
-		return fraseDao.getReferenceById(id);
-	}
-
-	@Override
-	public List<String> getTestoFrase() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String> getParoleInTesto() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int numeroParoleInFrase(String paroleInFrase) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	@Override
 	public void creaParola(String parola, Frase frase) {
 		Parola p = new Parola();
 		p.setTestoParola(parola);
@@ -97,5 +41,6 @@ public class ScriptServiceImpl implements ScriptService{
 		f.setTestoFrase(frase);
 		fraseDao.save(f);
 	  }
+
 }
 
