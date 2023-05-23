f4d8d6e93688b7a284ca07bc3d04a05096766e76
ufiore <119484794+ufiore@users.noreply.github.com>
2023-05-23 12:00:19 +0200

    1
    update di service e controllerRest

--- Prj_LearningByProject/src/main/java/com/example/integration/ControllerREST.java
+++ Prj_LearningByProject/src/main/java/com/example/integration/ControllerREST.java
@@ -10,22 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Frase;
 import com.example.entities.Parola;
-import com.example.services.FraseService;
-import com.example.services.ParolaService;
-import com.example.services.TestoService;
+import com.example.services.ScriptService;
+import com.example.services.ScriptServiceImpl;
 
 @RestController
 @RequestMapping ("api")
 public class ControllerREST {
 	
 	@Autowired
-	private ParolaService serviceParola;
-	private FraseService serviceFrase;
-	private TestoService serviceTesto;
+	private ScriptService serviceScript;
 	
 	@GetMapping("parola")
 	List<String> getParola(){
-		return serviceParola.getParola();
+		return
 	}
 	
 	@GetMapping("frase")
--- Prj_LearningByProject/src/main/java/com/example/services/ScriptService.java
+++ Prj_LearningByProject/src/main/java/com/example/services/ScriptService.java
@@ -9,13 +9,12 @@ public interface ScriptService {
 	
 	List<String> getParola();
 	Parola getParolaById (int id);
-	List<String> getTestoParola();
 	void creaParola(String parola, Frase frase);
 	
 	List<String> getFrase();
 	void creaFrase(String frase);
 	Frase getFraseById (int id);
-	List<String> getTestoFrase();
+	List<String> getFrasiInTesto();
 	List<String> getParoleInTesto();
 	int numeroParoleInFrase(String paroleInFrase);
 
