7802f8cd5a9a0fe936f8877ee86c1a6ac0ec8c48
srobITS <silvia.roberto@edu.itspiemonte.it>
2023-05-23 12:34:52 +0200

    ControllerREST

--- Prj_LearningByProject/src/main/java/com/example/integration/ControllerREST.java
+++ Prj_LearningByProject/src/main/java/com/example/integration/ControllerREST.java
@@ -1,58 +1,20 @@
 package com.example.integration;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.entities.Frase;
-import com.example.entities.Parola;
 import com.example.services.ScriptService;
-import com.example.services.ScriptServiceImpl;
 
 @RestController
-@RequestMapping ("api")
+@RequestMapping("api")
 public class ControllerREST {
 	
 	@Autowired
-	private ScriptService serviceScript;
+	private ScriptService service;
 	
-	@GetMapping("parola")
-	List<String> getParola(){
-		return
-	}
-	
-	@GetMapping("frase")
-	List<String> getFrase(){
-		return serviceFrase.getFrase();
-	}
-	
-	@GetMapping("testo")
-	List<String> getTesto(){
-		return serviceTesto.getTesto();
-	}
-
-	@GetMapping("parola/{id}")
-	Parola getProvincia(@PathVariable int id) {
-		return serviceParola.getParolaById(id);
-	}
-	
-	@GetMapping("frase/{id}")
-	Frase getFrase(@PathVariable int id) {
-		return serviceFrase.getFraseById(id); 
-	}
-	
-	@GetMapping ("testoParola")
-	List<String> getTestoParola(){
-		return serviceParola.getTestoParola();
-	}
-	
-	@GetMapping ("testoFrase")
-	List<String> getTestoFrase(){
-		return serviceFrase.getTestoFrase();
-	}
+	@GetMapping("parole")
+	public service.
 
 }
