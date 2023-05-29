e863a624a56519fbc6d360b99e66b143db844c9f
ufiore <119484794+ufiore@users.noreply.github.com>
2023-05-29 13:47:06 +0200

    Update ParolaDAO.java

--- Prj_LearningByProject/src/main/java/com/example/repos/ParolaDAO.java
+++ Prj_LearningByProject/src/main/java/com/example/repos/ParolaDAO.java
@@ -1,9 +1,11 @@
 package com.example.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.entities.Parola;
 
+@Repository
 public interface ParolaDAO extends JpaRepository<Parola, Integer>{
 
 }
